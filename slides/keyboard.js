0a6f21c2a444ba7636fd9067c2de5ffd323fcbdc
Brent Carmer <bcarmer@lavabit.com>
2013-11-15 18:36:32 -0800

    Slideshow!

---
+++ slides/keyboard.js
@@ -0,0 +1,21 @@
+$(function(){
+    var slideNum = parseInt(window.location.pathname.match("(\\d+)\\.html$")[1],10);
+    var totalSlides = 13;
+    console.log("slide " + slideNum + "/" + totalSlides);
+    $("body").on("keydown",function(e){
+        // 78 = "n"
+        if (e.keyCode == 78) {
+            var next = slideNum + 1;
+            if (next <= totalSlides) {
+                window.location.replace(next + ".html");
+            }
+        }
+        // 80 = "p"
+        else if (e.keyCode == 80) {
+            var prev = slideNum - 1;
+            if (prev > 0) {
+                window.location.replace(prev + ".html");
+            }
+        }
+    });
+});
