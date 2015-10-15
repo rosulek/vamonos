120fef37a0f70c2cb708edbf43bbb3b7bdece75b
Brent Carmer <bcarmer@gmail.com>
2015-10-15 16:13:27 -0700

    kejkajfkeajf

--- slides/keyboard.js
+++ slides/keyboard.js
@@ -3,15 +3,15 @@ $(function(){
     var totalSlides = 15;
     console.log("slide " + slideNum + "/" + totalSlides);
     $("body").on("keydown",function(e){
-        // 78 = "n"
-        if (e.keyCode == 78) {
+        // 39 = right arrow
+        if (e.keyCode == 39) {
             var next = slideNum + 1;
             if (next <= totalSlides) {
                 window.location.replace(next + ".html");
             }
         }
-        // 80 = "p"
-        else if (e.keyCode == 80) {
+        // 37 = left arrow
+        else if (e.keyCode == 37) {
             var prev = slideNum - 1;
             if (prev > 0) {
                 window.location.replace(prev + ".html");
