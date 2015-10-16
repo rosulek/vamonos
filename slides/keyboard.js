85d6e61e6c55995fa69c11172d809b327cdc2abe
Brent Carmer <carmerb@eecs.oregonstate.edu>
2015-10-16 10:44:26 -0700

    Fonts size on big ones

--- slides/keyboard.js
+++ slides/keyboard.js
@@ -4,14 +4,14 @@ $(function(){
     console.log("slide " + slideNum + "/" + totalSlides);
     $("body").on("keydown",function(e){
         // 39 = right arrow
-        if (e.keyCode == 39) {
+        if (e.keyCode == 39 && e.ctrlKey) {
             var next = slideNum + 1;
             if (next <= totalSlides) {
                 window.location.replace(next + ".html");
             }
         }
         // 37 = left arrow
-        else if (e.keyCode == 37) {
+        else if (e.keyCode == 37 && e.ctrlKey) {
             var prev = slideNum - 1;
             if (prev > 0) {
                 window.location.replace(prev + ".html");
